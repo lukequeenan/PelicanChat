9dda83a4d888a5b01dd8807e7e2c7f07b6592107
Luke Queenan <lukequeenan@gmail.com>
2011-03-12 13:44:27 -0800

    Started work on server.c

---
+++ server/server.c
@@ -0,0 +1,25 @@
+/*
+-- SOURCE FILE: server.c
+--
+-- PROGRAM: Pelican Chat Program
+--
+-- FUNCTIONS:
+-- int main(int argc , char *argv[]);
+--
+-- DATE: March 12, 2011
+--
+-- REVISIONS: (Date and Description)
+--
+-- DESIGNER: Luke Queenan
+--
+-- PROGRAMMER: Luke Queenan
+--
+-- NOTES:
+--
+*/
+
+int main (int argc, char **argv)
+{
+    return 0;
+}
+
