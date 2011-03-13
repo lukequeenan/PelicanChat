415f6117c3ae1207d9a0e8eb6ed3e9f768bb5f4b
Luke Queenan <lukequeenan@gmail.com>
2011-03-13 14:27:00 -0700

    Added some white space for formatting

--- network/network.h
+++ network/network.h
@@ -1,14 +1,17 @@
 #ifndef NETWORK_H
 #define NETWORK_H
+
 // Includes
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <strings.h>
+
 // Function Prototypes
 int tcpSocket();
 int setReuse(int* socket);
 int bindAddress(int *port, int *socket);
 int setListen(int *socket);
 int acceptConnection(int *listenSocket);
+
 #endif
