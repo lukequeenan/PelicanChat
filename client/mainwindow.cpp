5c24eadab19edc362d877d66b9cea02f4c66f60e
Luke Queenan <lukequeenan@gmail.com>
2011-03-13 21:35:24 -0700

    Added comments for the main window file.

--- client/mainwindow.cpp
+++ client/mainwindow.cpp
@@ -1,7 +1,52 @@
+/*
+-- SOURCE FILE: mainwindow.cpp
+--
+-- PROGRAM: Pelican Chat Program
+--
+-- FUNCTIONS:
+-- explicit MainWindow(QWidget *parent = 0);
+-- ~MainWindow();
+-- void on_action_Leave_Server_triggered();
+-- void on_action_Join_Server_triggered();
+--
+-- DATE: March 13, 2011
+--
+-- REVISIONS: (Date and Description)
+--
+-- DESIGNER: Luke Queenan
+--           Joel Stewart
+--
+-- PROGRAMMER: Luke Queenan
+--             Joel Stewart
+--
+-- NOTES:
+-- This file contains the information for the chat client program. All
+-- interactions are done though this window, which includes send and receiving
+-- messages, and connecting and disconnecting from the server.
+*/
+
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include "../network/network.h"
 
+/*
+-- FUNCTION: MainWindow
+--
+-- DATE: March 13, 2011
+--
+-- REVISIONS: (Date and Description)
+--
+-- DESIGNER: Qt
+--
+-- PROGRAMMER: Qt
+--
+-- INTERFACE: explicit MainWindow(QWidget *parent = 0);
+--
+-- RETURNS:
+--
+-- NOTES:
+-- This is the constructor for the main window of the chat client.
+*/
 MainWindow::MainWindow(QWidget *parent) :
         QMainWindow(parent),
         ui(new Ui::MainWindow)
@@ -9,6 +54,24 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->setupUi(this);
 }
 
+/*
+-- FUNCTION: ~MainWindow
+--
+-- DATE: March 13, 2011
+--
+-- REVISIONS: (Date and Description)
+--
+-- DESIGNER: Qt
+--
+-- PROGRAMMER: Qt
+--
+-- INTERFACE: ~MainWindow();
+--
+-- RETURNS:
+--
+-- NOTES:
+-- This is the destructor for the main window of the chat client.
+*/
 MainWindow::~MainWindow()
 {
     delete ui;
