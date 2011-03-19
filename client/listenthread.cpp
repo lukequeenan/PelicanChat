bca6f4f4e4e15fce0a1b62c5d31ff67774c876f9
Luke Queenan <lukequeenan@gmail.com>
2011-03-19 12:25:47 -0700

    Added comments for the listen thread.

--- client/listenthread.cpp
+++ client/listenthread.cpp
@@ -1,7 +1,47 @@
+/*
+-- SOURCE FILE: listenThread.cpp
+--
+-- PROGRAM: Pelican Chat Program
+--
+-- FUNCTIONS:
+-- void ListenThread::run();
+-- void ListenThread::setSocket(int *socket);
+--
+-- DATE: March 19, 2011
+--
+-- REVISIONS: (Date and Description)
+--
+-- DESIGNER: Luke Queenan
+--
+-- PROGRAMMER: Luke Queenan
+--
+-- NOTES:
+-- This file contains the thread functions for monitoring the socket for data.
+*/
+
 #include "listenthread.h"
 #include "../network/network.h"
 #include <stdlib.h>
 
+/*
+-- FUNCTION: run
+--
+-- DATE: March 19, 2011
+--
+-- REVISIONS: (Date and Description)
+--
+-- DESIGNER: Luke Queenan
+--
+-- PROGRAMMER: Luke Queenan
+--
+-- INTERFACE: void ListenThread::run();
+--
+-- RETURNS: void
+--
+-- NOTES:
+-- This is the run function for the thread. It monitors the socket for data and
+-- upon receiving a text message, sends it via a signal to the main program.
+*/
 void ListenThread::run()
 {
     int bytesRead = 0;
@@ -16,6 +56,24 @@ void ListenThread::run()
     }
 }
 
+/*
+-- FUNCTION: setSocket
+--
+-- DATE: March 13, 2011
+--
+-- REVISIONS: (Date and Description)
+--
+-- DESIGNER: Qt
+--
+-- PROGRAMMER: Qt
+--
+-- INTERFACE: void ListenThread::setSocket(int *socket);
+--
+-- RETURNS: void
+--
+-- NOTES:
+-- This allows the calling program to set the socket this thread is to monitor.
+*/
 void ListenThread::setSocket(int *socket)
 {
     mySocket_ = socket;
